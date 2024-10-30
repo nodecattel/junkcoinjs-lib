b3b139740ae2fa6a4209ce45a6a22d497c9eefce
Predmet <175954199+impredmet@users.noreply.github.com>
2024-10-30 17:51:24 -0400

    removed a useless todo

--- ts_src/payments/index.ts
+++ ts_src/payments/index.ts
@@ -12,9 +12,9 @@ import { p2ms } from './p2ms';
 import { p2pk } from './p2pk';
 import { p2pkh } from './p2pkh';
 import { p2sh } from './p2sh';
+import { p2tr } from './p2tr';
 import { p2wpkh } from './p2wpkh';
 import { p2wsh } from './p2wsh';
-import { p2tr } from './p2tr';
 
 export interface Payment {
   name?: string;
@@ -50,7 +50,4 @@ export type StackElement = Buffer | number;
 export type Stack = StackElement[];
 export type StackFunction = () => Stack;
 
-export { embed, p2ms, p2pk, p2pkh, p2sh, p2wpkh, p2wsh, p2tr };
-
-// TODO
-// witness commitment
+export { embed, p2ms, p2pk, p2pkh, p2sh, p2tr, p2wpkh, p2wsh };
